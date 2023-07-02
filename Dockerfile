0afc4cd2fbfbc00e6d7d62a282a4b3e642dd4659
CodeByKyle <kyle@codebykyle.com>
2023-07-03 03:59:46 +0700

    Push to docker hub

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM odoo:16.0
+
+COPY ./odoo/addons /mnt/extra-addons
+
--- odoo/addons/order_flow/__manifest__.py
+++ odoo/addons/order_flow/__manifest__.py
@@ -31,6 +31,6 @@
         ]
     },
     'installable': True,
-    'auto_install': False,
+    'auto_install': True,
     'application': True
 }
